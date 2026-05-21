0f2e2a6b64ec3910aa827ced7de41e5cf7bf1198
qwerty <162951673+year41@users.noreply.github.com>
2026-05-21 14:23:01 +1000

    Create basic book object constructor.

---
+++ script.js
@@ -0,0 +1,17 @@
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor.")
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = `${pages} pages`;
+    this.read = read === "yes" ? "already read" : "not read yet";
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    };
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not");
+const theOutsider = new Book("The Outsider", "Albert Camus", "144", "yes");
+console.log(theHobbit.info());
+console.log(theOutsider.info());
